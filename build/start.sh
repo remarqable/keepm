8d5b6d39411c2a9e18a18a2e267866a83dc37da8
dev@remarable.io <dev@remarqable.io>
2024-01-06 08:25:51 +0000

    Solved the empty postgres password issue

--- build/start.sh
+++ build/start.sh
@@ -2,13 +2,13 @@
 
 # Docker entrypoint script
 
-# Configure and start postgres server as postgres user
-su - postgres -c "chmod 0700 /var/lib/postgresql/data &&\
-initdb /var/lib/postgresql/data &&\
-echo \"host all  all    0.0.0.0/0  md5\" >> /var/lib/postgresql/data/pg_hba.conf &&\
-echo \"listen_addresses='*'\" >> /var/lib/postgresql/data/postgresql.conf &&\
-pg_ctl start -D /var/lib/postgresql/data &&\
-psql -U postgres -c \"CREATE DATABASE keepmdb;\""
+# Start PostgreSQL as the 'postgres' user and configure it
+su - postgres -c "chmod 0700 /var/lib/postgresql/data && \
+initdb /var/lib/postgresql/data && \
+echo \"host all all 0.0.0.0/0 md5\" >> /var/lib/postgresql/data/pg_hba.conf && \
+echo \"listen_addresses='*'\" >> /var/lib/postgresql/data/postgresql.conf && \
+pg_ctl start -D /var/lib/postgresql/data && \
+psql -c \"CREATE DATABASE keepmdb;\""
 
 # Load data
 psql -U postgres -d keepmdb -f /root/dbinit.sql
