194e7e573fb0838586557d4d6d6148d2abcc74db
dev@remarable.io <dev@remarqable.io>
2024-01-06 06:04:21 +0000

    Changed DB name

--- build/start.sh
+++ build/start.sh
@@ -8,7 +8,7 @@ initdb /var/lib/postgresql/data &&\
 echo \"host all  all    0.0.0.0/0  md5\" >> /var/lib/postgresql/data/pg_hba.conf &&\
 echo \"listen_addresses='*'\" >> /var/lib/postgresql/data/postgresql.conf &&\
 pg_ctl start -D /var/lib/postgresql/data &&\
-psql -U postgres -c \"CREATE DATABASE keepm\" &&\
+psql -U postgres -c \"CREATE DATABASE keepmdb\" &&\
 psql -U postgres -c \"CREATE USER keepm WITH PASSWORD 'keepm';\" &&\
 psql -U postgres -c \"grant all privileges on database keepm to keepm;\""
 
